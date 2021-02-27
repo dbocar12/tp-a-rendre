dc853cb680a4b79ebe88b1fd9c1554829ab782a5
dbocar12 <33195239+dbocar12@users.noreply.github.com>
2021-02-27 17:19:36 +0100

    added Joueur.java

---
+++ src/com/hakimbocar/Exo8/Joueur.java
@@ -0,0 +1,50 @@
+package com.hakimbocar.Exo8;
+
+import java.util.Objects;
+
+public class Joueur {
+    private String nom;
+    private int anneeDenaissance;
+
+    public Joueur() {
+    }
+
+    public Joueur(String nom, int anneeDenaissance) {
+        this.nom = nom;
+        this.anneeDenaissance = anneeDenaissance;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAnneeDenaissance() {
+        return anneeDenaissance;
+    }
+
+    public void setAnneeDenaissance(int anneeDenaissance) {
+        this.anneeDenaissance = anneeDenaissance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Joueur)) return false;
+        Joueur joueur = (Joueur) o;
+        return getAnneeDenaissance() == joueur.getAnneeDenaissance() && Objects.equals(getNom(), joueur.getNom());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNom(), getAnneeDenaissance());
+    }
+
+    @Override
+    public String toString() {
+        return " " + nom +" ";
+    }
+}
